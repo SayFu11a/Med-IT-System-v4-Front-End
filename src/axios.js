9eec424d508cfb3c06dd8fae8b38be4eb2b3c743
Islam Shagaev <islamshg@mail.ru>
2024-05-29 01:17:16 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+
+const baseURL = 'https://med-it-system-v4-back-end.onrender.com'
+// const baseURL = 'http://localhost:4444'
+
 const instance = axios.create({
-   baseURL: 'http://localhost:4444',
+   baseURL
 });
 
 instance.interceptors.request.use((config) => {
